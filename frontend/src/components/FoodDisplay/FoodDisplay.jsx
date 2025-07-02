51dc16e436fb993cbda1b18218161feb82eb452b
Uvere Ann <uvereann@gmail.com>
2025-07-02 20:23:16 +0100

    Added add to cart and remove from cart functionalities.

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
-import { food_list } from "../../assets/assets";
+//import { food_list } from "../../assets/assets";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
-  // const {food_list} = useContext(StoreContext);
+  const {food_list} = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,8 +7,8 @@ import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {/*<StoreContextProvider>*/}
+    <StoreContextProvider>
       <App />
-    {/*</StoreContextProvider>*/}
+    </StoreContextProvider>
   </BrowserRouter>
 );
