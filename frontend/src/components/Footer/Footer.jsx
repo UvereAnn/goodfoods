c57069fe8ed156a53c134c3f4eebb62f0c3ede89
Uvere Ann <uvereann@gmail.com>
2025-07-03 00:28:58 +0100

    added footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo image" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="facebook icon" />
+            <img src={assets.twitter_icon} alt="twitter icon" />
+            <img src={assets.linkedin_icon} alt="linkedin icon" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+2347068617694</li>
+            <li>contact@goodfoods.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        &copy; {new Date().getFullYear()} goodfoods.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
