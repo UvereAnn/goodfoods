bc337f557b1c7a67bc819659d4596f8d84024148
Uvere Ann <uvereann@gmail.com>
2025-07-05 14:44:53 +0100

    Added Navbar and Sidebar components and styles

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
+      </div>
+    </div>
   )
 }
 
