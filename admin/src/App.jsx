1fdf9526ea83631b45399bcfac754253b1062d9d
Uvere Ann <uvereann@gmail.com>
2025-07-05 16:40:37 +0100

    worked on add item and styling

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <hr/>
+      <Navbar />
+      <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
