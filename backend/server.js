8c06282a3f5dd9d767dc5de14625734d61a32ef9
Uvere Ann <uvereann@gmail.com>
2025-07-07 16:49:28 +0100

    Created user authentication (login & registration) feature

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
 
 const app = express();
 const port = 4000;
@@ -14,8 +16,9 @@ app.use(cors());
 connectDB();
 
 //api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
