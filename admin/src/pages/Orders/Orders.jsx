1c9c52dceaf7b30fed04a071911960c78180d5ce
Uvere Ann <uvereann@gmail.com>
2025-07-10 19:24:06 +0100

    Created order update feature

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -17,9 +17,20 @@ const Orders = ({ url }) => {
     }
   };
 
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
+
   return (
     <div className="order add">
       <h3>Order Page</h3>
@@ -57,7 +68,10 @@ const Orders = ({ url }) => {
               </div>
               <p>Items: {order.items.length}</p>
               <p>₦{order.amount}</p>
-              <select>
+              <select
+                onChange={(event) => statusHandler(event, order._id)}
+                value={order.status}
+              >
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out For Delivery">Out For Delivery</option>
                 <option value="Delivered">Delivered</option>
