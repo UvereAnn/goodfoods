79752beb312192d3e3e6eaf9879219bde299664a
Uvere Ann <uvereann@gmail.com>
2025-07-11 15:24:50 +0100

    Refactor: Remove unnecessary mongoose import from db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,8 +16,6 @@ export const connectDB = async () => {
   }
 };*/
 
-const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
