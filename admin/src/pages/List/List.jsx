eda2909a897292e747d1090f5210699280192d12
Ann Uvere <64098077+UvereAnn@users.noreply.github.com>
2025-07-11 01:23:42 +0100

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./List.css";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { url } from "../../assets/assets";
 
 const List = ({ url }) => {
   const [list, setList] = useState([]);
